d988c799bb2e65e4210d58bda9dbf975dced40ac
antaeseon <ridgnfu@naver.com>
2019-03-21 20:11:19 +0900

    modify

--- project/server/routes/users.js
+++ project/server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('강상연 바보');
 });
 
 module.exports = router;
