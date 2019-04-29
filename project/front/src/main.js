1e88d4c1fe2d40827eb77398c31639e9262a3274
antaeseon <ridgnfu@naver.com>
2019-04-29 21:08:29 +0900

    make store

--- project/front/src/main.js
+++ project/front/src/main.js
@@ -3,6 +3,7 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.prototype.$http=axios
@@ -10,5 +11,6 @@ Vue.prototype.$http=axios
 //fdfsdfes
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
---
+++ project/front/src/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    // 컴포넌트 간에 공유할 데이터 속성
+    state: {
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+        
+    }
+})
