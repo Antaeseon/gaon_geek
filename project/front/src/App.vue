0a3176c478b289fd17f9312fd73b9f0ecba6dbfd
antaeseon <ridgnfu@naver.com>
2019-05-06 02:51:06 +0900

    change main page

--- project/front/src/App.vue
+++ project/front/src/App.vue
@@ -22,14 +22,6 @@
           </v-list-tile-content>
         </v-list-tile>
 
-        <!-- isLogin이 false인 경우에만 왼쪽 바에서 로그인 버튼이 출력된다. -->
-        <v-list-tile router :to="{name: 'login'}">
-          <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>로그인</v-list-tile-content>
-        </v-list-tile>
-
         <!-- mypage router로 지시 -->
         <v-list-tile router :to="{name: 'mypage'}" exact>
           <v-list-tile-action>
