7fef3d403ae34d4e8bdb6ebef0b0d29954fdaabe
Jong in Park <whddls9632@naver.com>
2019-05-12 17:06:45 +0900

    attribute added

---
+++ project/front/src/attribute.js
@@ -0,0 +1,11 @@
+export default {
+    category: ['belt', 'cap', 'coat', 'dress', 'footwear', 'gown',
+        'jacket', 'jean', 'leggings', 'necklace', 'necktie',
+        'outdoor', 'pants', 'skirts', 'suits', 'sunglasses', 'sports wear', 'wallet', 'watch'
+    ],
+    size: ['S', 'M', 'L', 'XL', 'XXL', ],
+    tag: ['big collar', 'button', 'check', 'colorful', 'denim', 'dotted', 'embossing', 'floral', 'flower', 'heart', 'knit', 'lace', 'leaf',
+        'long', 'metal', 'neckline', 'off-shoulder', 'pastel', 'pattern', 'pleats', 'point', 'pocket', 'ribbon', 'see-through', 'small-collar',
+        'sleeved', 'sleeveless', 'surplice', 'stripe', 'tape', 'wrapping'
+    ]
+}
