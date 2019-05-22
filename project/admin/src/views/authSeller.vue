acdadf9594701a438b99a779eac751d2ea9b530c
antaeseon <ridgnfu@naver.com>
2019-05-22 21:24:59 +0900

    image change

--- project/admin/src/views/authSeller.vue
+++ project/admin/src/views/authSeller.vue
@@ -15,7 +15,6 @@
             <v-card-title class="headline grey lighten-2" primary-title>Auth Image</v-card-title>
             <v-img
               :src="'https://s3.ap-northeast-2.amazonaws.com/wearever1/'+props.item.imageUrl"
-              aspect-ratio="2.0"
             ></v-img>
             <v-divider></v-divider>
 
