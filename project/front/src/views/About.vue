d89128ff86d0e0919ca4cebfb45a93fc601c4383
Juho <einjuho7@ajou.ac.kr>
2019-05-27 20:50:36 +0900

    tempChange

--- project/front/src/views/About.vue
+++ project/front/src/views/About.vue
@@ -1,269 +1,76 @@
 <template>
-  <v-container
-    fluid
-    grid-list-xl
-    fill-height>
-    <v-layout
-      justify-center
-      align-center
+  <div>
+    <div>
+      <h2>Search and add a pin</h2>
+      <label>
+        <gmap-autocomplete
+          @place_changed="setPlace">
+        </gmap-autocomplete>
+        <button @click="addMarker">Add</button>
+      </label>
+      <br/>
+
+    </div>
+    <br>
+    <gmap-map
+      :center="center"
+      :zoom="12"
+      style="width:100%;  height: 400px;"
     >
-      <v-flex xs12>
-        <material-card
-          color="green">
-          <div
-            slot="header"
-          >
-            <div class="title font-weight-light mb-2">Notifications</div>
-            <div class="category">
-              Handcrafted by us with
-              <v-icon
-                size="17"
-              >
-                mdi-heart
-              </v-icon>
-            </div>
-          </div>
-
-          <v-card-text>
-            <v-layout
-              row
-              wrap>
-              <v-flex
-                md6
-                sm12>
-                <h2 class="title font-weight-light mb-3">Notifications Style</h2>
-
-                <material-notification
-                  class="mb-3"
-                  color="info"
-                >
-                  This is a plain notification
-                </material-notification>
-                <material-notification
-                  class="mb-3"
-                  color="info"
-                  dismissible
-                >
-                  This is a notification with close button.
-                </material-notification>
-                <material-notification
-                  class="mb-3"
-                  color="info"
-                  dismissible
-                  icon="mdi-bell-plus"
-                >
-
-                  This is a notification with close button and icon.
-                </material-notification>
-                <material-notification
-                  class="mb-3"
-                  color="info"
-                  dismissible
-                  icon="mdi-bell-plus"
-                >
-
-                  This is a notification with close button and icon and have many lines. You can see that the icon and the close button are always vertically aligned. This is a beautiful notification. So you don't have to worry about the style.
-                </material-notification>
-              </v-flex>
-
-              <v-flex
-                md6
-                sm12>
-                <h2 class="title font-weight-light">Notifcation States</h2>
-
-                <material-notification
-                  class="mb-3"
-                  color="info"
-                  dismissible
-                >
-                  <strong>INFO</strong> - This is a regular notification made with `color="info"`
-                </material-notification>
-
-                <material-notification
-                  class="mb-3"
-                  color="success"
-                  dismissible
-                >
-                  <strong>SUCCESS</strong> - This is a regular notification made with `color="success"`
-                </material-notification>
-
-                <material-notification
-                  class="mb-3"
-                  color="warning"
-                  dismissible
-                >
-                  <strong>WARNING</strong> - This is a regular notification made with `color="warning"`
-                </material-notification>
-
-                <material-notification
-                  class="mb-3"
-                  color="error"
-                  dismissible
-                >
-                  <strong>DANGER</strong> - This is a regular notification made with `color="error"`
-                </material-notification>
-
-                <material-notification
-                  class="mb-3"
-                  color="purple"
-                  dismissible
-                >
-                  <strong>PRIMARY</strong> - This is a regular notification made with `color="purple"`
-                </material-notification>
-              </v-flex>
-            </v-layout>
-            <div class="text-xs-center">
-              <h2 class="title font-weight-light mb-2">Notification Places</h2>
-              <span class=" subheading font-weight-light grey--text">Click to view notifications</span>
-            </div>
-
-            <v-container grid-list-lg>
-              <v-layout
-                justify-center
-                row
-                wrap>
-                <v-flex
-                  xs12
-                  sm4>
-                  <v-btn
-                    block
-                    color="success"
-                    @click="snack('top', 'left')"
-                  >
-                    Top Left
-                  </v-btn>
-                </v-flex>
-                <v-flex
-                  xs12
-                  sm4>
-                  <v-btn
-                    block
-                    color="success"
-                    @click="snack('top')"
-                  >
-                    Top Center
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-              <v-layout
-                justify-center
-                row
-                wrap>
-                <v-flex
-                  xs12
-                  sm4>
-                  <v-btn
-                    block
-                    color="success"
-                    @click="snack('top', 'right')"
-                  >
-                    Top Right
-                  </v-btn>
-                </v-flex>
-                <v-flex
-                  xs12
-                  sm4>
-                  <v-btn
-                    block
-                    color="success"
-                    @click="snack('bottom', 'left')"
-                  >
-                    Bottom Left
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-              <v-layout
-                justify-center
-                row
-                wrap>
-                <v-flex
-                  xs12
-                  sm4>
-                  <v-btn
-                    block
-                    color="success"
-                    @click="snack('bottom')"
-                  >
-                    Bottom Center
-                  </v-btn>
-                </v-flex>
-                <v-flex
-                  xs12
-                  sm4>
-                  <v-btn
-                    block
-                    color="success"
-                    @click="snack('bottom', 'right')"
-                  >
-                    Bottom Right
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-container>
-
-            <v-snackbar
-              :color="color"
-              :bottom="bottom"
-              :top="top"
-              :left="left"
-              :right="right"
-              v-model="snackbar"
-              dark
-            >
-              <v-icon
-                color="white"
-                class="mr-3"
-              >
-                mdi-bell-plus
-              </v-icon>
-              <div>Welcome to <b>Vue Material Dashboard</b> - a beautiful freebie for every web developer.</div>
-              <v-icon
-                size="16"
-                @click="snackbar = false"
-              >
-                mdi-close-circle
-              </v-icon>
-            </v-snackbar>
-          </v-card-text>
-        </material-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        @click="center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    color: null,
-    colors: [
-      'purple',
-      'info',
-      'success',
-      'warning',
-      'error'
-    ],
-    top: true,
-    bottom: false,
-    left: false,
-    right: false,
-    snackbar: false
+  name: "GoogleMap",
+   data: () => ({
+
+     // default to Montreal to keep it simple
+      // change this to whatever makes sense
+      center: { lat: 45.508, lng: -73.587 },
+      markers: [ ],
+      places: [],
+      currentPlace: null
+    
   }),
+ 
+  mounted() {
+    this.geolocate();
+  },
 
   methods: {
-    snack (...args) {
-      this.top = false
-      this.bottom = false
-      this.left = false
-      this.right = false
-
-      for (const loc of args) {
-        this[loc] = true
+    // receives a place object via the autocomplete component
+    setPlace(place) {
+      this.currentPlace = place;
+    },
+    addMarker() {
+      if (this.currentPlace) {
+     
+        const marker = {
+          lat: this.currentPlace.geometry.location.lat(),
+          lng: this.currentPlace.geometry.location.lng()
+        };
+        this.markers.push({ position: marker });
+        this.places.push(this.currentPlace);
+        this.center = marker;
+        this.currentPlace = null;
       }
-
-      this.color = this.colors[Math.floor(Math.random() * this.colors.length)]
-
-      this.snackbar = true
+    },
+    geolocate: function() {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      });
     }
   }
-}
+};
 </script>
