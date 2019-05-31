4fd5b3477115dd9dab434a6f6bf562bcb184dddd
Jong in Park <whddls9632@naver.com>
2019-06-01 01:13:19 +0900

    detail Bug Fix

--- project/front/src/views/detail.vue
+++ project/front/src/views/detail.vue
@@ -16,92 +16,7 @@
               </v-tab-item>
               <v-tab-item>
                 <v-card flat>
-                  <v-card-text>
-                    {{ qna }}
-                    <div>
-    <v-dialog v-model="dialog" max-width="500px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark class="mb-2" v-on="on">글쓰기</v-btn>
-        <v-btn
-          color="primary"
-          dark
-          @click="expand = !expand"
-        >{{ expand ? 'Close' : 'Keep' }} other rows</v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">{{ formTitle }}</span>
-        </v-card-title>
-
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6 md12>
-                <v-text-field v-model="editedItem.userid" label="작성자"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md12>
-                <v-text-field v-model="editedItem.date" label="작성일"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md12>
-                <v-text-field
-                  outline
-                  v-model="editedItem.title"
-                  label="제목"
-                  value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md12>
-                <v-textarea
-                  outline
-                  v-model="editedItem.content"
-                  label="내용"
-                  value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-                ></v-textarea>
-              </v-flex>
-              
-              
-            </v-layout>
-          </v-container>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-          <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      class="elevation-1"
-      :expand="expand"
-      item-key="title"
-    >
-      <template v-slot:items="props">
-        <tr @click="props.expanded = !props.expanded">
-             
-        <td class="text-xs-left">{{ props.item.userid }}</td>
-        <td class="text-xs-left">{{ props.item.date }}</td>
-        <td>{{ props.item.title }}</td>
-        
-        <td class="text-xs-left">
-          <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
-          <v-icon small @click="deleteItem(props.item)">delete</v-icon>
-        </td>
-        </tr>
-      </template>
-      <template v-slot:expand="props">
-        <v-card flat>
-          <v-card-text><h3>판매 내용</h3>{{ props.item.content }}</v-card-text>
-        </v-card>
-      </template>
-    </v-data-table>
-  </div>
-               
-    
-                  </v-card-text>
+                  <v-card-text>{{ qna }}</v-card-text>
                 </v-card>
               </v-tab-item>
               <v-tab-item>
@@ -140,7 +55,6 @@
                         <h2>상품명 : {{mainItem.item_name}}</h2>
                         <br>
                         판매가격 : {{mainItem.price}}
-                        
                         <br>
                         1일당 : {{mainItem.rental}}
                         <br>
@@ -168,7 +82,7 @@
                           <v-flex xs3>
                             <v-select :items="['렌탈', '구매']" label="구매방법" v-model="statusFilter"></v-select>
                           </v-flex>
-                            
+
                           <v-menu
                             ref="menu"
                             v-model="menu"
@@ -253,56 +167,10 @@
 <script>
 import Vue from "vue";
 import axios from "axios";
-import board from '../components/Board'
 
 export default {
-  
   data() {
     return {
-     expand: false,
-    dialog: false,
-    headers: [
-        
-      //{ text: "content내용", value: "content" },
-      { text: "작성자", value: "userid" },
-      { text: "작성일", value: "date" },
-      //{ text: "Protein (g)", value: "protein" },
-      {
-        text: "제목",
-        align: "left",
-        sortable: false,
-        value: "title"
-      } ,
-      { text: "수정/삭제", sortable: false }
-    ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      title: "제목없음",
-      content: "내용없음",
-      userid: this.$store.state.id,
-      //userid: "id없음",
-      date: new Date().toISOString().substr(0, 10)
-     // protein: 0
-    },
-    defaultItem: {
-      title: "제목없음",
-      content: "내용없음",
-      //userid: "id없음",
-      userid: this.$store.state.id,
-      date: new Date().toISOString().substr(0, 10)
-      //protein: 0
-    },
-      
-      isDark: true,
-      show: true,
-      newTodo: "",
-      todo: [],
-      todos: [],
-      day: this.todoDay(),
-      //date: new Date().getDate(),
-      ord: this.nth(new Date().getDate()),
-      year: new Date().getFullYear(),
       tradeList: [],
       pagaItem: [],
       mainItem: {},
@@ -318,23 +186,8 @@ export default {
       daylength: null,
       statusFilter: "",
       t_method: "",
-      t_price: 0
+      t_price:0
     };
-  },
-  components: {
-      board
-  },
-   
-computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "글쓰기" : "수정하기";
-    }
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    }
   },
   async created() {
     console.log("여여여여", this.$route.params.id);
@@ -357,82 +210,6 @@ computed: {
   },
 
   methods: {
-     
-
-      editItem (item) {
-        this.editedIndex = this.desserts.indexOf(item)  
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        } else {
-          this.desserts.push(this.editedItem)
-        }
-        this.close()
-      },
-    addTodo() {
-      var value = this.newTodo && this.newTodo.trim();
-      if (!value) {
-        return;
-      }
-      this.todos.push({
-        title: this.newTodo,
-        done: false
-      });
-      this.newTodo = "";
-    },
-    removeTodo(index) {
-      this.todos.splice(index, 1);
-    },
-    todoDay() {
-      var d = new Date();
-      var days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-      ];
-      return days[d.getDay()];
-    },
-    nth(d) {
-      if (d > 3 && d < 21) return "th";
-      switch (d % 10) {
-        case 1:
-          return "st";
-        case 2:
-          return "nd";
-        case 3:
-          return "rd";
-        default:
-          return "th";
-      }
-    }
-  },
-  filters: {
-    capitalize: function(value) {
-      if (!value) return "";
-      value = value.toString();
-      return value.charAt(0).toUpperCase() + value.slice(1);
-    },
     submit() {
       //보내고 싶은 번호와 메세지
       this.$http.post("http://localhost:3000/sens/sendMessage", {
@@ -449,7 +226,7 @@ computed: {
         return_date: this.datesend,
         is_buy: false,
         trade_method: this.t_method,
-        total_price: this.t_price
+        total_price:this.t_price
       });
     },
     computeDate() {
@@ -465,52 +242,49 @@ computed: {
       );
       this.daylength = between;
     },
-    allowedDates: function(val) {
-      console.log("gg");
-      console.log(val);
-      var temp = val.substring(0, 10).split("-");
-      var temp_date = temp[0] + temp[1] + temp[2];
-      console.log(this.tradeList.length);
-      for (var i = 0; i < this.tradeList.length; i++) {
+    allowedDates: function(val) { 
+        console.log('gg')
+        console.log(val)
+        var temp = val.substring(0, 10).split("-");
+        var temp_date = temp[0] + temp[1] + temp[2];
+        console.log(this.tradeList.length)
+        for (var i = 0; i < this.tradeList.length; i++) {
+          if(this.tradeList[i].borrow_date==null||this.tradeList[i].return_date==null)
+            continue
+          var sarray = this.tradeList[i].borrow_date.substring(0, 10).split("-");
+          var tarray = sarray[0] + sarray[1] + sarray[2];
+          var endarray = this.tradeList[i].return_date
+            .substring(0, 10)
+            .split("-");
+          var tearray = endarray[0] + endarray[1] + endarray[2];
+          if (temp_date >= tarray && temp_date <= tearray) return false;
+        }
         if (
-          this.tradeList[i].borrow_date == null ||
-          this.tradeList[i].return_date == null
+          parseInt(val.substring(5, 7)) <
+          parseInt(new Date().toISOString().substring(5, 7))
         )
-          continue;
-        var sarray = this.tradeList[i].borrow_date.substring(0, 10).split("-");
-        var tarray = sarray[0] + sarray[1] + sarray[2];
-        var endarray = this.tradeList[i].return_date
-          .substring(0, 10)
-          .split("-");
-        var tearray = endarray[0] + endarray[1] + endarray[2];
-        if (temp_date >= tarray && temp_date <= tearray) return false;
-      }
-      if (
-        parseInt(val.substring(5, 7)) <
-        parseInt(new Date().toISOString().substring(5, 7))
-      )
-        return false;
-      else if (
-        parseInt(val.substring(5, 7)) >
-        parseInt(new Date().toISOString().substring(5, 7))
-      )
-        return true;
-      if (
-        parseInt(val.split("-")[2], 10) >
-        parseInt(
-          new Date()
-            .toISOString()
-            .substr(0, 10)
-            .split("-")[2],
-          10
+          return false;
+        else if (
+          parseInt(val.substring(5, 7)) >
+          parseInt(new Date().toISOString().substring(5, 7))
         )
-      )
-        return true;
+          return true;
+        if (
+          parseInt(val.split("-")[2], 10) >
+          parseInt(
+            new Date()
+              .toISOString()
+              .substr(0, 10)
+              .split("-")[2],
+            10
+          )
+        )
+          return true;
     },
     requestPay: function() {
-      if (this.$store.state.Token == null) {
-        alert("로그인을 해 주세요.");
-        return;
+      if(this.$store.state.Token==null){
+        alert("로그인을 해 주세요.")
+        return
       }
       // IMP.request_pay(param, callback) 호출
       var totalPrice;
@@ -526,9 +300,9 @@ computed: {
           alert("날짜 설정이 잘못되었습니다.");
           return;
         }
-        totalPrice = this.mainItem.rental * (this.daylength + 1);
+        totalPrice = this.mainItem.rental * (this.daylength+1);
       }
-      this.t_price = totalPrice;
+      this.t_price=totalPrice
       Vue.IMP().request_pay(
         {
           pg: "html5_inicis",
@@ -563,9 +337,3 @@ computed: {
   }
 };
 </script>
-<style>
-.done {
-  text-decoration: line-through;
-}
-</style>
-
